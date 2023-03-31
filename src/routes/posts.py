731d255c579cff113cbcc03d8f691e5c8d771406
Maria-hub746 <113303447+Maria-hub746@users.noreply.github.com>
2023-03-31 21:35:23 +0300

    routes posts

---
+++ src/routes/posts.py
@@ -0,0 +1,117 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+from fastapi_limiter.depends import RateLimiter
+
+from src.database.connect_db import get_db
+from src.database.models import User
+from src.schemas import PostModel, PostUpdate, PostStatusUpdate, PostResponse
+from src.repository import posts as repository_posts
+from src.services.auth import auth_service
+
+router = APIRouter(prefix='/posts', tags=["posts"])
+
+
+@router.get("/all", response_model=List[PostResponse], description='No more than 10 requests per minute',
+            dependencies=[Depends(RateLimiter(times=10, seconds=60))])
+async def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),
+                     current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_posts(skip, limit, current_user, db)
+    return posts
+
+
+@router.get("/by_id{post_id}", response_model=PostResponse)
+async def read_post(post_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    post = await repository_posts.get_posts(post_id, current_user, db)
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return post
+
+
+@router.get("/by_title/{post_title}", response_model=PostResponse)
+async def read_post(post_title: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_posts_by_title(post_title, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.get("/by_user_id/{user_id}", response_model=PostResponse)
+async def read_post(user_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_posts_by_user_id(user_id, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.get("/by_username/{user_name}", response_model=PostResponse)
+async def read_post(user_name: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_posts_by_username(user_name, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.get("/with_hashtag/{hashtag_name}", response_model=PostResponse)
+async def read_post(hashtag_name: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_posts_with_hashtag(hashtag_name, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.get("/comments/all/{post_id}", response_model=PostResponse)
+async def read_post(post_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_comments(post_id, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.get("hashtags/all/{post_id}", response_model=PostResponse)
+async def read_post(post_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_hashtags(post_id, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.get("/rating/{post_id}", response_model=PostResponse)
+async def read_post(post_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    posts = await repository_posts.get_rating(post_id, current_user, db)
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return posts
+
+
+@router.post("/", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+async def create_post(body: PostModel, db: Session = Depends(get_db),
+                      current_user: User = Depends(auth_service.get_current_user)):
+    return await repository_posts.create_post(body, current_user, db)
+
+
+@router.put("/{post_id}", response_model=PostResponse)
+async def update_post(body: PostUpdate, post_id: int, db: Session = Depends(get_db),
+                      current_user: User = Depends(auth_service.get_current_user)):
+    post = await repository_posts.update_post(post_id, body, current_user, db)
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return post
+
+
+@router.delete("/{post_id}", response_model=PostResponse)
+async def remove_post(post_id: int, db: Session = Depends(get_db),
+                      current_user: User = Depends(auth_service.get_current_user)):
+    post = await repository_posts.remove_post(post_id, current_user, db)
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return post
