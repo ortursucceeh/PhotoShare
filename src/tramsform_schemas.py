75274dbee4791241ded972211b967627a2a34984
Vasyl-Hlushchenko <gluschenkov88@gmail.com>
2023-04-08 01:15:43 +0300

    upgr cloud

--- src/tramsform_schemas.py
+++ src/tramsform_schemas.py
@@ -2,11 +2,13 @@ from pydantic import BaseModel, Field
 
 
 class TransformCircleModel(BaseModel):
+    usefull: bool = False
     height: int = Field(ge=0, default=400)
     width: int = Field(ge=0, default=400)
 
 
 class TransformEffectModel(BaseModel):
+    usefull: bool = False
     art_audrey: bool = False
     art_zorro: bool = False
     blur: bool = False
@@ -14,6 +16,7 @@ class TransformEffectModel(BaseModel):
     
 
 class TransformResizeModel(BaseModel):
+    usefull: bool = False
     crop: bool = False
     fill: bool = False
     height: int = Field(ge=0, default=400)
@@ -21,5 +24,13 @@ class TransformResizeModel(BaseModel):
 
 
 class TransformTextModel(BaseModel):
+    usefull: bool = False
     font_size: int = Field(ge=0, default=70)
     text: str = Field(max_length=100, default="")
+
+
+class TransformBodyModel(BaseModel):
+    circle: TransformCircleModel
+    effect: TransformEffectModel
+    resize: TransformResizeModel
+    text: TransformTextModel
